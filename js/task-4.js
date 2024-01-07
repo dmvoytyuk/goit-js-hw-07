612f6516dd18507868dc548021c115ee73a70606
dmvoytyuk <dmvoytyuk@gmail.com>
2024-01-07 12:39:59 +0100

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+const userData = {};
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (
+    !event.target.elements.email.value ||
+    !event.target.elements.password.value
+  ) {
+    alert('All form fields must be filled in');
+  } else {
+    userData[event.target.elements.email.getAttribute('name')] =
+      event.target.elements.email.value.trim();
+    userData[event.target.elements.password.getAttribute('name')] =
+      event.target.elements.password.value.trim();
+  }
+  console.log(userData);
+  event.target.reset();
+});
