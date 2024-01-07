6a5e8262c8c8c7596e5a1300221f184bf7bcd336
dmvoytyuk <dmvoytyuk@gmail.com>
2024-01-07 11:50:53 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,11 @@
-const 
+const inputField = document.getElementById('name-input');
+const nameField = document.getElementById('name-output');
+console.log(inputField);
+
+inputField.addEventListener('input', event => {
+  if (inputField.value.trim()) {
+    nameField.textContent = inputField.value.trim();
+  } else {
+    nameField.textContent = 'Anonymous';
+  }
+});
