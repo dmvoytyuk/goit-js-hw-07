88a724c7ce48cff6dcd2b9b6c885b530a62e17ec
dmvoytyuk <dmvoytyuk@gmail.com>
2024-01-07 14:27:44 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesContainer = document.getElementById('boxes');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const inputField = document.querySelector('input');
+
+console.log(inputField);
+
+// console.log(createButton);
+// console.log(destroyButton);
+// console.log(boxesContainer);
+
+function createBoxes(amount) {}
+function validateInput(value) {
+  console.log(value);
+  value = Number.parseInt(value);
+  console.log(value);
+  if (isNaN(value)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+destroyButton.addEventListener('click', event => {
+  boxesContainer.innerHTML = '';
+});
+
+createButton.addEventListener('click', event => {
+  console.log(validateInput(inputField.value));
+});
