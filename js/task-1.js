da8b45e856771c05685b1a4bb46ea678e7160738
dmvoytyuk <dmvoytyuk@gmail.com>
2024-01-07 10:40:07 +0100

    task1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categories = document.querySelectorAll('.item');
+console.log(`number of categories: ${categories.length}`);
+console.log(categories);
+
+categories.forEach(category => {
+  const categoryTitle = category.querySelector('h2');
+  const categoryElements = category.querySelectorAll('li');
+  console.log(`Category: ${categoryTitle.textContent}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
